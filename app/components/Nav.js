c0128e43148c3ccf767103baca185b14ba92fa46
rzhancock <rzhancock@gmail.com>
2018-10-09 01:41:15 -0700

    production build working. created back button

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -2,6 +2,13 @@ var React = require('react');
 var NavLink= require('react-router-dom').NavLink;
 
 function Nav () {
+
+	function goBack(e){
+		e.preventDefault();
+		history.back();
+		return false;
+	}
+
 	return (
 		<ul className='gb-nav'>
 			<li>
@@ -13,6 +20,9 @@ function Nav () {
 				<NavLink activeClassName='active' to='/github-battle/popular'>
 					Popular
 				</NavLink>
+			</li>
+			<li className='goBack'>
+				<a href='#' onClick={goBack} >&#8656;Return to previous page</a>
 			</li>
 		</ul>
 	)
